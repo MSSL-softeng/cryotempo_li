5a30f5c76df95b3c03dff59ad4dbc9296dd5d72a
Alan Muir <a.muir@ucl.ac.uk>
2024-05-23 14:25:00 +0100

    updated pdocs_build

--- pdocs_build.sh
+++ pdocs_build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-exit 0
+# Get the current Git branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+# Check if the current branch is master
+if [ "$current_branch" = "master" ]; then
+echo "on $current_branch branch"
 pdoc -o docs src/clev2er --no-include-undocumented --mermaid --logo "https://www.homepages.ucl.ac.uk/~ucasamu/mssl_logo.png" --docformat google 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -9,5 +13,9 @@ if [ $retVal -ne 0 ]; then
     exit 1
 fi
 git add docs
+
+else
+    echo "Not on master branch, skipping pdocs build."
+fi
 
 exit 0
