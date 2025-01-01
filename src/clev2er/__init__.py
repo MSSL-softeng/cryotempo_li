aadd9d096361cbde07438a942fc293e0f705eaf6
Alan Muir <a.muir@ucl.ac.uk>
2025-01-01 22:25:40 +0000

    docs update

--- src/clev2er/__init__.py
+++ src/clev2er/__init__.py
@@ -12,7 +12,7 @@ workflow. There are many run-time options in the chain controller command line t
 
 The diagram below shows a simplified representation of the framework and its components.
 
-![framework](images/framework.jpg "framework")
+![framework](images/framework.jpg "framewrk")
 
 ## Main Features
 
